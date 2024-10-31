766d97ea8a58deb831caf8cffaa1e063bdbe3ce1
Carolina Rosatto <carolrosatto@gmail.com>
2024-10-31 20:17:32 -0300

    Configuração inicial

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+package org.example
+
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest.newBuilder
+import java.net.http.HttpResponse.BodyHandlers
+
+
+fun main() {
+    val client: HttpClient = HttpClient.newHttpClient()
+    val request = newBuilder()
+        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=612"))
+        .build()
+    val response = client
+        .send(request, BodyHandlers.ofString())
+
+    val jsonResponse = response.body()
+    println(jsonResponse)
+}
